23ccdfe796468bebbfb3c540eaaec8c8a7b76f78
Craig Giddings <randomhero_345@hotmail.com>
2025-09-18 21:32:21 -0400

    Adding chapter 16 projects

---
+++ ch15/projects/p03/qsort.c
@@ -0,0 +1,29 @@
+/* Sorts an array of integers using Quicksort algorithm*/
+
+#include <stdio.h>
+#include "quicksort.h"  
+
+#define N 7
+
+void quicksort(int a[], int low, int high);
+int split(int a[], int low, int hight);
+
+int main(void) {
+
+    int a[N], i;
+
+    printf("Enter %d numbers to be sorted: ", N);
+    for (i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    quicksort(a, 0, N - 1);
+
+    printf("In sorted order: ");
+    for (i = 0; i < N; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
