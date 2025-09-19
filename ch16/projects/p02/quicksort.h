98ab74b696513b9d0de8c4ca774ba496d4b2b8bf
Craig Giddings <randomhero_345@hotmail.com>
2025-09-18 21:31:38 -0400

    finished ch15 projects and started chapter 16 projects

---
+++ ch16/projects/p02/quicksort.h
@@ -0,0 +1,9 @@
+/* quicksort, edited for struct part */
+
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+void quicksort(struct part a[], int low, int high);
+int split(struct part a[], int low, int high);
+
+#endif
